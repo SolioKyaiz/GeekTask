c6e1686519332fbcaa988d145585ff1092199e53
SolioKyaiz <jonesbonked@gmail.com>
2025-03-29 18:05:18 +0600

    postman commit

--- task/urls.py
+++ task/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('',TaskListAPIView.as_view(),name = 'task_list'),
-    path('create/',TaskCreateAPIView.as_view(),name = 'plant_create'),
-    path('update/<int:pk>/',TaskUpdateAPIView.as_view(),name = 'plant_update'),
-    path('delete/<int:pk>/',TaskDeleteAPIView.as_view(),name = 'plant_delete'),
-    path('<int:pk>/',TaskDetailAPIView.as_view(),name = 'plant_detail'),
+    path('create/',TaskCreateAPIView.as_view(),name = 'task_create'),
+    path('update/<int:pk>/',TaskUpdateAPIView.as_view(),name = 'task_update'),
+    path('delete/<int:pk>/',TaskDeleteAPIView.as_view(),name = 'task_delete'),
+    path('<int:pk>/',TaskDetailAPIView.as_view(),name = 'task_detail'),
 
 ]
